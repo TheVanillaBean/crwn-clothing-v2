e099a9981c47631b1d1966491f1ed1ffd5e3a84c
Alex Alimov <aalimov@asu.edu>
2022-05-27 22:22:32 -0700

    Scaffold basic homepage

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './components/navigation/navigation.component';
+import Home from './routes/home/home.component';
+
+const Shop = () => {
+  return <h1>fsdf</h1>;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
