da0503376ba0397258c67996dd1820471177069c
Alex Alimov <aalimov@asu.edu>
2022-06-04 15:18:16 -0700

    implmenent redux reducer for user

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,9 +1,4 @@
-import { createContext, useEffect, useReducer } from 'react';
-import {
-  createUserDocumentFromAuth,
-  onAuthStateChangedListener,
-} from '../utils/firebase/firebase.utils';
-import { createAction } from '../utils/reducer/reducer.utils';
+import { createContext } from 'react';
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -11,19 +6,14 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
-export const USER_ACTION_TYPES = {
-  SET_CURRENT_USER: 'SET_CURRENT_USER',
-};
-
 const userReducer = (state, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      throw new Error(`Unhandled type ${type} in userReducer`);
-  }
+  // const { type, payload } = action;
+  // switch (type) {
+  //   case USER_ACTION_TYPES.SET_CURRENT_USER:
+  //     return { ...state, currentUser: payload };
+  //   default:
+  //     throw new Error(`Unhandled type ${type} in userReducer`);
+  // }
 };
 
 const INITIAL_STATE = {
@@ -31,27 +21,20 @@ const INITIAL_STATE = {
 };
 
 export const UserProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
-
-  const { currentUser } = state;
-
-  const setCurrentUser = (user) => {
-    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
-  };
-
-  const value = { currentUser, setCurrentUser };
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-
-      setCurrentUser(user);
-    });
-
-    return unsubscribe;
-  }, []);
-
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+  // const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  // const { currentUser } = state;
+  // const setCurrentUser = (user) => {
+  //   dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+  // };
+  // const value = { currentUser, setCurrentUser };
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChangedListener((user) => {
+  //     if (user) {
+  //       createUserDocumentFromAuth(user);
+  //     }
+  //     setCurrentUser(user);
+  //   });
+  //   return unsubscribe;
+  // }, []);
+  // return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,18 @@
 import { useContext } from 'react';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import CartDropDown from '../../components/cart-dropdown/cart-dropdown.component';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import { CartContext } from '../../contexts/cart.context';
-import { UserContext } from '../../contexts/user.context';
+import { setCurrentUser } from '../../store/user/user.actions';
+import { selectCurrentUser } from '../../store/user/user.selectors';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import './navigation.styles.jsx';
 import { LogoContainer, NavigationContainer, NavLink, NavLinks } from './navigation.styles.jsx';
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
   // console.log(currentUser);
 
